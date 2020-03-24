1f22de2e02fd319635667c256fcfb54fb9a95fcb
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-03-24 13:28:32 +0530

    added spinner

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends Component{
   //   loading: false})
   // }
   searchUsers= async text =>{
+    this.setState({loading: true});
     const res= await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     console.log(res);
     this.setState({users: res.data.items,
